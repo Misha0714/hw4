e5031fddc8d5c5d4a7ece7e683ce9ab36f3a43ba
Misha Faruki <mfaruki@usc.edu>
2024-03-14 15:35:06 -0700

    equal-paths.cpp changes

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -8,11 +8,52 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
+int depth(Node* root) {
+    int right = 0; 
+    int left = 0; 
+    if(root == NULL) { 
+        return 0; 
+    }
+    depth(root->left); 
+    left+=1; 
+    depth(root->right); 
+    right+=1; 
 
+
+}
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    //if root equals null 
+    if (root == NULL) {
+        return true; 
+    }
+    //if root does not have a left or right node 
+    if (root->left == NULL && root->right == NULL) {
+        return true; 
+    }
+    int right = 0; 
+    int left = 0; 
+    //call equalPaths on left side of tree 
+    bool leftPath = equalPaths(root->left);  
+    //call equalPaths on left side of tree 
+    bool rightPath = equalPaths(root->right); 
+
+    if (root->right != nullptr) {
+        right++; 
+    }
+    if (root->left!= nullptr) {
+        left++; 
+    }
+
+    if(right==left && leftPath && rightPath) {
+        reutrn true; 
+    }
+    else [
+        return false; 
+    ]
+
 
 }
 
